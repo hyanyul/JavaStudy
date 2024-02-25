2a9db3f7da1201843e0502b899e42fb585f18e61
hyanyul <tjdwl0808@gmail.com>
2024-02-25 21:57:55 +0900

    장바구니 v1 찐막 커밋

--- practice/src/buy/Customer.java
+++ practice/src/buy/Customer.java
@@ -6,7 +6,7 @@ public class Customer {
 	int id;		//회원 번호 필드
 	String name;	//회원명 필드
 	int age;	//회원 나이 필드
-	Product cart[];	//회원의 장바구니 필드, 선택하는 상품을 담을 수 있어야 하기 때문에 배열 형태로 선언
+	Product cart[];	//회원의 장바구니 필드, 선택하는 상품을 담을 수 있어야 하기 때문에 배열 형태로 선언->다음 버전에서 ArrayList으로 변경해볼 것
 	
 	Customer(int id, String name, int age, Product cart[]){	//회원 등록용 생성자
 		this.id = id;	//생성자 통해 필드 초기화
@@ -15,7 +15,7 @@ public class Customer {
 		this.cart = cart;
 	}
 	
-	public void customerInfo() {	//회원 등록 시 확인 문구 출력용 메소드, 생성자 안에 프린트 함수 사용 시 객체 사용할 때마다 출력되는듯?
+	public void customerInfo() {	//회원 등록 시 확인 문구 출력용 메소드
 		System.out.printf("[회원 등록] '회원 번호: %d, 이름: %s, 나이: %d세'가 등록되었습니다.\n", id, name, age);
 	}
 	
